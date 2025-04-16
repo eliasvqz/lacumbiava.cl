f3d82c9b45502208f7679502bf6e22a22474bf49
ChileNetwork <msotosalgado.77@gmail.com>
2025-04-16 12:59:05 -0400

    Mensaje de commit desde CODE!!!

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,4 +5,10 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
     //
+    public $saludo = "Hola desde el controlador base";
+    public function __construct()
+    {
+        // Constructor de la clase base
+        // Puedes inicializar propiedades o realizar acciones comunes aquí
+    }
 }
